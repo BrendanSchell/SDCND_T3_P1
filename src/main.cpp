011de5d1dba8bf9868e5af791d7cbbb87efa2975
Brendan Schell <brendanschell1@gmail.com>
2017-08-05 10:44:28 -0400

    Improved lane following:
    * Added helper function to get d coordinate for lanes
    * Added high resolution map waypoints for smoother trajectory

--- src/main.cpp
+++ src/main.cpp
@@ -160,6 +160,25 @@ vector<double> getXY(double s, double d, vector<double> maps_s, vector<double> m
 
 }
 
+double getD(int lane_id)
+{
+	return (lane_id)*4+2;
+}
+
+int getLaneId(int d)
+{
+	if (d >= 8 && d <= 12) 
+	{
+		return 2;
+	}
+	else if (d >= 4 && d <= 8)
+	{
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
 int main() {
   uWS::Hub h;
 
@@ -197,7 +216,26 @@ int main() {
   	map_waypoints_dy.push_back(d_y);
   }
 
-  h.onMessage([&map_waypoints_x,&map_waypoints_y,&map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
+  tk::spline map_sx;
+  tk::spline map_sy;
+  map_sx.set_points(map_waypoints_s,map_waypoints_x);
+  map_sy.set_points(map_waypoints_s, map_waypoints_y);
+
+  vector<double> hires_map_waypoints_x;
+  vector<double> hires_map_waypoints_y;
+  vector<double> hires_map_waypoints_s;
+  
+  double cur_s = 0; 
+  while (cur_s < map_waypoints_s[map_waypoints_s.size()-1])
+  {
+	  hires_map_waypoints_x.push_back(map_sx(cur_s));
+	  hires_map_waypoints_y.push_back(map_sy(cur_s));
+	  hires_map_waypoints_s.push_back(cur_s);  
+	  cur_s += 1;
+  }
+
+
+  h.onMessage([&hires_map_waypoints_x,&hires_map_waypoints_y,&hires_map_waypoints_s,&map_waypoints_dx,&map_waypoints_dy](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length,
                      uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
@@ -240,13 +278,18 @@ int main() {
           	vector<double> next_y_vals;
 			
 			// try fitting spline to next waypoint
-			int wp = NextWaypoint(car_x, car_y, car_yaw,map_waypoints_x,map_waypoints_y);
+			int wp = NextWaypoint(car_x, car_y, car_yaw,hires_map_waypoints_x,hires_map_waypoints_y);
 
-			double dist_inc = 0.005;
+			double dist_inc = 0.44;
 
 			tk::spline s;
 			
-			int path_size = 0;
+			vector<double> spline_s;
+			vector<double> spline_d;
+
+			int path_size = previous_path_x.size();
+
+			int n_points = 50;
 
 			double pos_x;
 			double pos_y;
@@ -260,16 +303,56 @@ int main() {
 				pos_x = previous_path_x[path_size-1];
 				pos_y = previous_path_y[path_size-1];
 
+				double pos_x2 = previous_path_x[path_size-2];
+				double pos_y2 = previous_path_y[path_size-2];
+				angle = atan2(pos_y-pos_y2,pos_x-pos_x2);
 			}
-			for (int i = previous_path_x.size()-25; i < previous_path_x.size(); i++){
+			for (int i = 0; i < path_size; i++){
 					next_x_vals.push_back(previous_path_x[i]);
 					next_y_vals.push_back(previous_path_y[i]);
 
 			}
+			auto pos_frenet = getFrenet(pos_x, pos_y, angle, hires_map_waypoints_x, hires_map_waypoints_y);
+			spline_s.push_back(pos_frenet[0]);
+			spline_d.push_back(pos_frenet[1]);
 
-			for (int i = 0; i < 75-path_size; i++){
-				pos_x += (map_waypoints_x[wp]+6*map_waypoints_dx[wp]-pos_x)*(dist_inc*i);
-				pos_y += (map_waypoints_y[wp]+6*map_waypoints_dy[wp]-pos_y)*(dist_inc*i);
+			double middle_lane_d = getD(1);
+
+			for (int i =0; i < 100;i++){
+
+				if (hires_map_waypoints_s[wp+i] > pos_frenet[0]){
+					spline_s.push_back(hires_map_waypoints_s[wp+i]);
+					spline_d.push_back(middle_lane_d);
+				}
+			}
+
+			if (spline_s.size() <= 3){
+
+				for (int i = 0; i < spline_s.size(); i++){
+					spline_s[i] -= hires_map_waypoints_s[hires_map_waypoints_s.size()-1];
+				}
+				for (int i = 0; i < 100 - spline_s.size();i++){
+					spline_s.push_back(hires_map_waypoints_s[i]);
+					spline_d.push_back(middle_lane_d);
+				}
+			}
+			s.set_points(spline_s,spline_d);
+
+			double pos_s = spline_s[0];
+			double pos_d = spline_d[0];	
+			for (int i = 0; i < n_points-path_size; i++){
+				//pos_x += (map_waypoints_x[wp]+6*map_waypoints_dx[wp]-pos_x)*(dist_inc*i);
+				//pos_y += (map_waypoints_y[wp1+6*map_waypoints_dy[wp]-pos_y)*(dist_inc*i);
+				pos_s += dist_inc;
+			    pos_d = s(pos_s);	
+				vector<double> next_xy;
+				if (pos_s < 0){
+					next_xy = getXY(pos_s+hires_map_waypoints_s[hires_map_waypoints_s.size()-1],pos_d,hires_map_waypoints_s,hires_map_waypoints_x, hires_map_waypoints_y);
+				}else{
+					next_xy = getXY(pos_s,pos_d,hires_map_waypoints_s,hires_map_waypoints_x,hires_map_waypoints_y); 
+				}
+				pos_x = next_xy[0];
+				pos_y = next_xy[1]; 
 
 				next_x_vals.push_back(pos_x);
 				next_y_vals.push_back(pos_y);
